588d85cff23a6af116eeec02d1917727db2ff340
beratsvm160 <berat.svm@hotmail.com>
2022-12-08 13:35:14 +0300

    odev8

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const axios = require("axios");
 var apiSecenekleri = {
-  sunucu: "https://mekanbul8.beratsevim.co",
+  sunucu: "https://mekanbul8.beratsevim.repl.co",
   apiYolu: "/api/mekanlar/",
 };
 var mesafeyiFormatla = function (mesafe) {
